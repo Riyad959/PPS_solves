822f03d3c089d8cb69561e3ad98411107a7bb6fe
Riyadul Islam <112506658+Riyad959@users.noreply.github.com>
2022-12-05 10:13:48 +0600

    1st_commit

---
+++ 1197.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+	int t, v, saida;
+
+	scanf("%d %d", &v, &t);
+
+	do
+	{
+		saida = (t*2) * v;
+		printf("%d\n", saida);
+	} while ( scanf("%d %d", &v, &t) != EOF );
+
+	return 0;
+}
